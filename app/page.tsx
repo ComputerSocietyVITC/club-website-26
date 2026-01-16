0b095411c22d2ef8bbe17fae27c0d93596ce81fd
Parth Verma <vermaparth2409@gmail.com>
2026-01-16 22:42:38 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import Sponsor from './components/Sponsor';
-
-export default function SponsorPreview() {
-  return (
-    <main className="min-h-screen bg-black flex items-center justify-center">
-      <Sponsor />
-    </main>
-  );
+export default function Home() {
+  return <div>HOME</div>
 }
